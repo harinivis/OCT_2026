86f1d6aaaa65cace2ea4dc97e2e6890f62e0655b
harinivis <105524028+harinivis@users.noreply.github.com>
2026-02-17 12:00:19 -0500

    Add files via upload

--- OCT_Data_Into_Board.m
+++ OCT_Data_Into_Board.m
@@ -7,6 +7,12 @@ octData = load("C:\Users\Harin\OneDrive\Capstone\768_synthetic_interferograms_bi
 interferograms = octData.interferograms;
 data_bytes = typecast(interferograms, 'int32');
 
+
+%  Open a UDP port on the PC
+%The udpport object allows you to perform byte-type and datagram-type UDP communication using a UDP socket on the local host.
+u = udpport("datagram", "IPV4"); %IPV4 is an IP address version
+
+
 % Send to FPGA IP address and port
 fpga_ip = "###.###.#.##";  % FPGA IP 
 fpga_port = 5000;           % UDP port the FPGA is listening on
@@ -21,7 +27,7 @@ for i = 1:768
     Message_Type = int8(0); %To say this is data packet or a calibration packet (0 for data, 1 for calib) int8 = holds 1 byte which is 0 - 255
     ScanNumber = int16(i); %1st A-scan? 2nd A-scan? etc
     NumberofSamples = int16(length(scan)); %should ve 512 samples per A scan (the number of samples for that A scan)
-    Data = (scan); %the actual interferrogram data
+    Data = int32(scan); %the actual interferrogram data
     Close = int16(32767);
     
     %Packet structure
@@ -32,6 +38,22 @@ for i = 1:768
 
   % Display a message indicating the completion of the current scan
    disp(['Scan ', num2str(i), ' yaaas']);
+
+   %CRC16 https://www.mathworks.com/help/comm/ref/crcgenerate.html . codeword = crcGenerate(msg,crcCfg) 
+    %https://itoolkit.co/blog/2023/08/what-is-the-difference-between-crc32-vs-crc16/
+    %https://eureka.patsnap.com/article/how-cyclic-redundancy-check-crc-32-vs-crc-16-is-implemented-in-hardware
+    %https://www.numberanalytics.com/blog/crc-in-fpga-design-best-practices
+   
+    %int2bit = converts integers to bits https://www.mathworks.com/help/comm/ref/int2bit.html
+
+    Data_bits = int2bit(Data(:), 32); %32 bits for each integer
+    %Data_bits = Data_bits(:); %need to make column vector cuz crcGenerate wants column of vector bits (N X 1)
+    Data_bits = reshape(Data_bits.', [], 1);
+
+    %codeword = crcGenerate(msg, crcCfg); %generates CRC checksums for the input message based on the specified CRC congfiguration object. The function appends the CRC checksums to the input message
+    codeword = crcGenerate(Polynomial = " 0x8005"); 
+
+
 
 end
 
