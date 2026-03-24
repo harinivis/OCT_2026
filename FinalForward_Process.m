debf89c74bed81b9ebec4dc6e865e0aaff76f644
harinivis <105524028+harinivis@users.noreply.github.com>
2026-03-24 13:07:30 -0400

    Add files via upload

--- FinalForward_Process.m
+++ FinalForward_Process.m
@@ -3,81 +3,115 @@ clear variables
 clc
 
 %OCT Pipeline
-%Step 1: Load the OCT data
-files = dir('forward_pipeline_outputs_scaled*.mat'); %get the mat files in the 1 folder it is located in
+% Step 1: Load the OCT data
+
+%files = dir('forward_pipeline_outputs_optimized*.mat'); %get the mat files in the 1 folder it is located in
+files = dir('raw_signals_optimal*.bin');
 
 for i = 1:length(files)
-        filename= files(i).name;
+    filename = files(i).name;
+    [~,~,ext] = fileparts(filename);  % check if mat or bin file
+
+    if strcmp(ext, '.mat') %if its a matfile
         octData = load(filename);
-        bscan_data_real = octData.raw_signals_real;
-        bscan_data_im = octData.raw_signals_im;
-        bscan_data = bscan_data_real + 1i .* bscan_data_im; % Combine real and imaginary parts
+        bscan_data_real = real(octData.raw_signals);
+        
+
+        % get dimensions
+        [num_ascans, num_depth] = size(bscan_data_real);  % e.g., 768 x 1024
+        signal_length = num_depth;
+
+        %calib data
+        tvec = octData.tvec;
+        kclk = octData.kclk;
+        mybg = octData.mybg;
+        maxmin = octData.maxmin;
+        a_coeffs = octData.a_coeffs;
     
-%raw_signals = 768 x 512 complex double 
-%column is num of data points in 1 A line (496)(there are 768 a lines)
-%[row, column]
+    elseif strcmp(ext, '.bin') %if bin file
+        fid = fopen(filename,'rb');
 
-%bscan_data = bscan_data.;   % <-- MAKE IT MATCH REVERSE
-[num_ascans, num_depth] = size(bscan_data); % [768x512]
-signal_length = num_depth;
-%need to make a loop: for i = 1:NumberofAscans
-%then implement below formulas for each A scan at a time
+     
 
-% Define Sampling Grid
-%make sure to run once
+        % specify the dimensions of your data (must match how the bin was saved)
+        num_ascans = 768;   % e.g., number of A-lines
+        num_depth  = 1024;  % e.g., points per A-line
+        signal_length = num_depth;
 
- tvec = octData.tvec;
-% kclk = 0.05 * rand(1,signal_length) - 0.025;
-% kclk = tvec + kclk;
-% kclk(1) = 0;
+        % read raw data   
+        raw = fread(fid, 'int32'); 
+        fclose(fid);
+
+        % reshape into [num_ascans x num_depth]
+         bscan_data_real = reshape(raw, num_ascans, num_depth);  
+        num_depth = size(bscan_data_real, 2);
+           
 
 
-kclk = octData.kclk;
+       
+        calibData = load('calibration_data.mat', 'tvec', 'kclk', 'mybg', 'maxmin', 'a_coeffs');
 
-% OCT PARAMETERS
-maxmin      = [800e-9, 900e-9];
-a_coeffs    = [0, -4 * 10^-11, 0];
-mynoise = 0.01 * randn(1, signal_length); % gaussian/white noise with mean = 0, var = 0.01
-mybg = octData.mybg;
-% STORAGE
-%og_sig  = complex(zeros(signal_length, num_ascans));
-b_recon = complex(zeros(num_ascans, signal_length));
+       
+        tvec    = calibData.tvec;
+        kclk    = calibData.kclk;
+        mybg    = calibData.mybg;
+        maxmin  = calibData.maxmin;
+        a_coeffs = calibData.a_coeffs;
+
+
+    else
+        error('boi get the right file types');
+    end
+
+
+
+
+%% OCT PARAMETERS
+
+
+half_depth = floor(signal_length / 2); %remove half the depth
+b_recon = (zeros(num_ascans, half_depth));
 % mybg = zeros(signal_length,1);
 % mybg = 100000 * sin(tvec * 0.003);
-
-
-
     
-    % FORWARD PIPELINE (reconstruct)
+    % FORWARD PIPELINE (reconstruct) %IF USING PARFOR, U CANNOT USE i as
+    % INDEX ITS LITERALLY A RULE
     tic
-    for i = 1:num_ascans %(i = 1:768)
-        raw = bscan_data(i, :); %(i, :) is going through every A scan and getting all its data points being 512
+    parfor j = 1:num_ascans %(i = 1:768)
+        raw = bscan_data_real(j, :); %(i, :) is going through every A scan and getting all its data points being 512
+        %raw = hilbert(bscan_data_real(i,:)); %used to reconstruct a complex signal from real interferrograms (used to get a complex analytical signal)
         s1 = bg_sub_f(raw);               % Remove background
         s2 = k_lin_f(s1, tvec, kclk);           % Linearize k-space
+        %s2 = k_lin_f(s1, kclk);
         s3 = disp_c_f(s2, maxmin, a_coeffs);   % Correct dispersion
         s4 = fft_f(s3);                         % FFT to spatial domain
+        s4 = s4(1:floor(end/2)); 
+        
+      
         s5 = log_sc_f(s4);                      % Log scale magnitude
         s6 = gs_map_f (s5);
-        b_recon(i, :) = s6;
-    toc
-   
-
+        b_recon(j, :) = s6;
+        
+    
     end
     
+toc
+ 
      % Display 
     figure;
     %imagesc(20*log10(abs(b_recon +  1e-12)))
     % threshold = 1e-80;                  % values below this are considered background
     % b_recon(abs(b_recon) < threshold) = 0;
-    b_recon_smooth = imgaussfilt(abs(b_recon), 0.2); % sigma = 1
-    imagesc(abs(b_recon_smooth).')
+    %b_recon_smooth = imgaussfilt(abs(b_recon), 0.2); % sigma = 1
+    imagesc(abs(b_recon).')
+
+    axis image
     colormap(gray)
-    title('B scan')
-   
+    title('Final B scan')
+
      
 end
-
-
+toc
 
     %Functions used
     
@@ -112,16 +146,34 @@ end
         % Forward: Map from nonlinear k-clock to linear k-space
          % assert(length(nonlinear_y) == length(nonlinear_x), ...
          %     'k_lin_f: signal and source grid must match');
-        output = interp1(nonlinear_x, nonlinear_y, linear_x, 'cubic');
+        output = interp1(nonlinear_x, nonlinear_y, linear_x, 'pchip');
+
     end
-    
-    
+    % function output = k_lin_f(signal, kclk)
+    % k_linear = linspace(min(kclk), max(kclk), length(kclk));
+    % output = interp1(kclk, signal, k_linear, 'linear', 0);
+    % end
+    % 
     % FFT
-    function output = fft_f(input)
-        % Forward: FFT to spatial domain
-        output = fft(input);
-    end
     
+function output = fft_f(input)
+    output = fft(input);
+end
+
+    % 
+    % function output = fft_f(input)
+    %     % Forward: FFT to spatial domain
+    % 
+    % 
+    % % FFT
+    % 
+    % output = fft(input);
+    % %output = abs(output);
+    % 
+    % %output  = fft(temp);
+    % 
+    % end
+    % 
     
     % lOG Scaling
     function output = log_sc_f(input)
@@ -129,7 +181,7 @@ end
         % Input: complex data from FFT
         % Output: real dB magnitude (magnitude domain)
         %epsv = 1e-12;   
-        output = 20*log10((input ));
+        output = 20*log10(abs(input ));
     end
     
     
@@ -145,8 +197,6 @@ end
        
     end
     
+
     
  
-
-
-
